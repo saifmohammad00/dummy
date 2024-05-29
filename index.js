f825d797b9fefd10f9ff80c66a9c3eee41c6117b
saifmohammad00 <saifmohammad592001@gmail.com>
2024-05-29 13:40:55 +0530

    sixth commit

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-const mainheading=document.querySelector('#main-heading');
-mainheading.style.textAlign='right';
-const fruits=document.querySelector('.fruits');
-fruits.style.background='grey';
-fruits.style.padding='30px';
-fruits.style.borderRadius='5px';
-const allfruits=document.querySelectorAll('.fruit');
-for(let s of allfruits){
-  s.style.margin='5px';
-  s.style.padding='5px';
-  s.style.listStyleType='none';
-  s.style.backgroundColor='white';
-  s.style.borderRadius='5px'
-}
-// Write answer to the questions asked below:
-const basketheading=document.querySelector('#basket-heading');
-basketheading.style.color='brown';
-const evenfruit=document.querySelectorAll('.fruit:nth-child(even)');
-for(let s of evenfruit){
-  s.style.backgroundColor='brown';
-  s.style.color='white';
-}
+const subheading=document.createElement('h3');
+const text=document.createTextNode('Buy high quality organic fruits online');
+subheading.appendChild(text);
+const divs=document.querySelectorAll('div');
+divs[0].appendChild(subheading);
+subheading.style.fontStyle='italic';
+const para=document.createElement('p');
+const text2=document.createTextNode('Total fruits: 4');
+para.appendChild(text2);
+divs[1].insertBefore(para,divs[1][1]);
+para.setAttribute('id','fruits-total');
