51173a496bf7c1719c3486b2fd60ab820ad4397e
saifmohammad00 <saifmohammad592001@gmail.com>
2024-05-29 13:14:42 +0530

    fifth commit

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const list=document.getElementsByTagName('li');
-list[4].style.color='blue';
-for(let x of list){
-  x.style.fontStyle='italic';
+const mainheading=document.querySelector('#main-heading');
+mainheading.style.textAlign='right';
+const fruits=document.querySelector('.fruits');
+fruits.style.background='grey';
+fruits.style.padding='30px';
+fruits.style.borderRadius='5px';
+const allfruits=document.querySelectorAll('.fruit');
+for(let s of allfruits){
+  s.style.margin='5px';
+  s.style.padding='5px';
+  s.style.listStyleType='none';
+  s.style.backgroundColor='white';
+  s.style.borderRadius='5px'
+}
+// Write answer to the questions asked below:
+const basketheading=document.querySelector('#basket-heading');
+basketheading.style.color='brown';
+const evenfruit=document.querySelectorAll('.fruit:nth-child(even)');
+for(let s of evenfruit){
+  s.style.backgroundColor='brown';
+  s.style.color='white';
 }
