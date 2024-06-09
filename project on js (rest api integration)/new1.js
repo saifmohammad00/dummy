a83a3840413991cdb4493bbf9912b8f47ee14e3c
saifmohammad00 <saifmohammad592001@gmail.com>
2024-06-09 19:03:56 +0530

    commit

--- project on js (rest api integration)/new1.js
+++ project on js (rest api integration)/new1.js
@@ -1,47 +1,47 @@
 document.querySelector('form').addEventListener("submit",function(e){
     e.preventDefault();
-    if(e.target.name.value==="" || e.target.des.value===""){
+    const [x,y]=["name","des"].map(i=>e.target[i].value);
+    if(x==="" || y===""){
         document.querySelector('.error-message').style.display="block";
         return;
     }
     else{
         document.querySelector('.error-message').style.display="none";
     }
-    axios.post("https://crudcrud.com/api/fa01d525468047d8b4131ca043c5c6fe/folder",{
-         "para":e.target.name.value+"-"+e.target.des.value,
+    axios.post("https://crudcrud.com/api/7ff2d42375054d5789119abc7b2eab1d/folder",{
+         "para":x+"-"+y,
          "flag":true
 
     })
     .then((res)=>{
-        const data=`<li uid="${res.data._id}">
-                <p>${res.data.para}</p>
-                <button class="done">done</button>
+        const data=`<li uid="${res.data._id}" data-x="${res.data.para}">
+                ${res.data.para}
+                <button class="done">Done</button>
                 <button class="delete">Delete</button>
                 </li>`
         document.querySelector('.todo').innerHTML+=data;
     })
     .catch((err)=>{console.log(err)})
-    e.target.name.value="";
-    e.target.des.value="";
+    e.target.reset();
 })
-document.querySelector('.todo').addEventListener("click",function(s){
+document.querySelector('.list').addEventListener("click",function(s){
     if(s.target.classList.contains('delete')){
-        axios.delete(`https://crudcrud.com/api/fa01d525468047d8b4131ca043c5c6fe/folder/${s.target.parentElement.getAttribute('uid')}`)
+        axios.delete(`https://crudcrud.com/api/7ff2d42375054d5789119abc7b2eab1d/folder/${s.target.parentElement.getAttribute('uid')}`)
         .then((res)=>{
             s.target.parentElement.remove();
         })
-        .then((err)=>{console.log(err)})
+        .catch((err)=>{console.log(err)})
     }
     else if(s.target.classList.contains('done')){
-        axios.put(`https://crudcrud.com/api/fa01d525468047d8b4131ca043c5c6fe/folder/${s.target.parentElement.getAttribute('uid')}`,{
-            "para":s.target.parentElement.querySelector('p').textContent,
+        axios.put(`https://crudcrud.com/api/7ff2d42375054d5789119abc7b2eab1d/folder/${s.target.parentElement.getAttribute('uid')}`,{
+            "para":s.target.parentElement.dataset.x,
             "flag":false
         })
         .then((res)=>{
-            const data = `<li uid="${s.target.parentElement.getAttribute('uid')}">
-                    <p>${s.target.parentElement.querySelector('p').textContent}</p>
-                    <button class="delete">Delete</button>
-                    </li>`;
+            const data=`<li uid="${s.target.parentElement.getAttribute('uid')}" data-x="${s.target.parentElement.dataset.x}">
+                        ${s.target.parentElement.dataset.x}
+                        <button class="delete">Delete</button>
+                        </li>`
             document.querySelector('.complete').innerHTML += data;
             s.target.parentElement.remove();
         })
@@ -51,31 +51,24 @@ document.querySelector('.todo').addEventListener("click",function(s){
     }
 })
 window.addEventListener("DOMContentLoaded",function(event){
-    axios.get("https://crudcrud.com/api/fa01d525468047d8b4131ca043c5c6fe/folder")
+    axios.get("https://crudcrud.com/api/7ff2d42375054d5789119abc7b2eab1d/folder")
     .then((res)=>{
         res.data.forEach((item) => {
             if (item.flag) {
-                const data = `<li uid="${item._id}">
-                    <p>${item.para}</p>
-                    <button class="done">done</button>
-                    <button class="delete">Delete</button>
-                    </li>`;
+                const data = `<li uid="${item._id}" data-x="${item.para}">
+                                ${item.para}
+                                <button class="done">Done</button>
+                                <button class="delete">Delete</button>
+                                </li>`;
                 document.querySelector('.todo').innerHTML += data;
             } else {
-                const data = `<li uid="${item._id}">
-                    <p>${item.para}</p>
-                    <button class="delete">Delete</button>
-                    </li>`;
+                const data = `<li uid="${item._id}" data-x="${item.pata}">
+                                ${item.para}
+                                <button class="delete">Delete</button>
+                                </li>`;
                 document.querySelector('.complete').innerHTML += data;
             }
         });
-    })
-    .catch((err)=>{console.log(err)})
-})
-document.querySelector('.complete').addEventListener("click",function(g){
-    axios.delete(`https://crudcrud.com/api/fa01d525468047d8b4131ca043c5c6fe/folder/${g.target.parentElement.getAttribute('uid')}`)
-    .then((res)=>{
-        g.target.parentElement.remove();
     })
     .catch((err)=>{console.log(err)})
 })
